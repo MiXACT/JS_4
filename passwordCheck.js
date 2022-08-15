ffd7dc46f4db3f6d88bebc347975cfb5fd6936c3
MiXACT <out_lawz@mail.ru>
2022-08-15 18:51:18 +0300

    passwordChecker_workout_2

--- passwordCheck.js
+++ passwordCheck.js
@@ -1,16 +1,16 @@
 function getPasswordChecker(password) {
-    function validatePass(userPass) {
-        const validPass = '777';
-        let checkStatus = false;
-        if (userPass === validPass) checkStatus = true;
-        return checkStatus;
-    }
-    return validatePass(password);
+	return function(truePass) {
+		let status = false;
+		if (truePass === password) status = true;
+		return status;
+	}
 }
 
 
-const falsePass = getPasswordChecker('wrong_password');
-const truePass = getPasswordChecker('777');
+const passChecker = getPasswordChecker('777');
+console.log(`TRUE_777 = ${passChecker('777')}`);
+console.log(`FALSE_555 = ${passChecker('555')}`);
 
-console.log(`falsePass is ${falsePass} password!`);
-console.log(`truePass is ${truePass} password!`);
+const passChecker2 = getPasswordChecker('555');
+console.log(`TRUE_555 = ${passChecker2('555')}`);
+console.log(`FALSE_777 = ${passChecker2('777')}`);
